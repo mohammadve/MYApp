6ba91f1f13b90f6208d3f2df00d8156a80cd3b73
mohammad.yunus <mohammad@virtualemployee.com>
2019-04-04 16:12:21 +0530

    Project setup

---
+++ MYApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYApp
+//
+//  Created by Mohammad Yunus on 04/04/19.
+//  Copyright © 2019 VE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
