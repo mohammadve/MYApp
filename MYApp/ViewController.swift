fb84a9600c065d0558cac4160a425b649926a7c7
Mohammad Yunus <mohammad@virtualemployee.com>
2019-07-12 15:15:00 +0530

    Testing

--- MYApp/ViewController.swift
+++ MYApp/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
         print("Hello")
 	}
+    
+    func callrequest() {
+        print("making an API call")
+    }
 
 
 }
